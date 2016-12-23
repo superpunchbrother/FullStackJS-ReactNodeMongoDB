feb842227d2f8ba91064c8d72f7e2db5542cad6c
Tim <superpunchbrother@gmail.com>
2016-12-23 15:13:20 -0600

    api added

--- config.js
+++ config.js
@@ -9,10 +9,5 @@ export const logStars = function(message) {
 };
 
 export default {
-  mongodbUri: 'mongodb://localhost:27017/test',
-  port: env.PORT || 8080,
-  host: env.HOST || '0.0.0.0',
-  get serverUrl() {
-    return `http://${this.host}:${this.port}`;
-  }
+  port: env.PORT || 8080
 };
